59a7c3bbaccab921e4aaa9c0259fb223f166ae56
vallinoto <vallinoto@gmail.com>
2023-10-15 11:02:42 -0300

    feat: crud post  finished

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from datetime import datetime, date
 
 
 class Post(models.Model):
@@ -8,10 +9,10 @@ class Post(models.Model):
     title_tag = models.CharField(max_length=255, default="TAG Default")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
+    post_date  = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
     
     def get_absolute_url(self):
-        # return reverse('article-detail', args=(str(self.id)))
         return reverse('home')
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,14 @@
 # from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+from .forms import PostForm, EditForm
+from django.urls import reverse_lazy
 
 
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    ordering = ['-post_date']
 
 
 class ArticleDetailView(DetailView):
@@ -15,5 +18,18 @@ class ArticleDetailView(DetailView):
 
 class AddPostView(CreateView):
     model = Post
+    form_class = PostForm
     template_name = 'add_post.html'
-    fields = '__all__'
+
+
+class UpdatePostView(UpdateView):
+    model = Post
+    form_class = EditForm
+    template_name = 'update_post.html'
+    # fields = ['title','title_tag','author','body']
+
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
