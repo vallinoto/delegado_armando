dab52e48392a055e9c6de4d2c15e5f676abd46d7
vallinoto <vallinoto@gmail.com>
2023-11-04 16:05:49 -0300

    enable ricktext input

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from ckeditor.fields import RichTextField 
 
 
 
@@ -29,7 +30,8 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255, default="TAG Default")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.TextField()
+    body = RichTextField(blank=True, null=True)
+    # body = models.TextField()
     post_date  = models.DateField(auto_now_add=True)
     category = models.CharField(max_length=255, default="Uncategorize")
 
--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     "blog",
     "members",
+    "ckeditor",
 ]
 
 MIDDLEWARE = [
